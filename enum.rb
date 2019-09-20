5dc756fab4eddce5944e957769fcbb52ec84c70f
noomdalv <vladlunasan@gmail.com>
2019-09-20 12:41:32 -0500

    added methods up to .my_none?

--- enum.rb
+++ enum.rb
@@ -9,9 +9,65 @@ def my_each
   self
 end
 
+def my_each_with_index
+    i = 0
+    while i < self.size
+      yield(self[i], i)
+      i += 1
+    end
+    self
+  end
+
+
+  def my_select
+    i = 0
+    new_array = []
+    while i < self.size
+      if yield(self[i]) == true
+        new_array << self[i]
+      end
+      i += 1
+    end
+    new_array
+  end
+
+  def my_all?
+    i = 0
+    while i < self.size
+      if yield(self[i]) == false || yield(self[i]) == nil
+        return false
+      end
+      i += 1
+    end
+    true
+  end
+
+  def my_any?
+    i = 0
+    while i < self.size
+      if yield(self[i])
+        return true
+      end
+      i += 1
+    end
+    false
+  end
+
+  def my_none?
+		i = 0
+		while i < self.size
+			if yield(self[i])
+				return false
+			end
+			i += 1
+		end
+		true
+  end
+
+
+
 end
 
-#EXAMPLES
-
-#my_each
-#["a", "b", "c"].my_each { |char| puts "#{char}" }
+#test
+#numbers = [1, 2, 3, 4, 10]
+#puts numbers.my_none { |x| x < 11 }
