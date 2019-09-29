ec41c677e3238dbe42e1ec94130310822396eceb
noomdalv <vladlunasan@gmail.com>
2019-09-29 10:46:23 -0500

    fixed my_inject

--- enum.rb
+++ enum.rb
@@ -107,28 +107,59 @@ module Enumerable
   end
 
   def my_inject(accumulator = nil, symbol = nil)
+	  array = to_a
     if !accumulator.nil? && !symbol.nil?
-      my_each { |num| accumulator = accumulator.method(symbol).call(num) }
+      array.my_each { |num| accumulator = accumulator.method(symbol).call(num) }
       accumulator
     elsif !accumulator.nil? && accumulator.is_a?(Symbol) && symbol.nil?
-      memo, *remaining_elements = self
-      remaining_elements.my_each { |num| memo = memo.method(accumulator).call(num) }
+		  memo = array.shift
+      array.my_each { |num| memo = memo.method(accumulator).call(num) }
       memo
     elsif !accumulator.nil? && accumulator.is_a?(Integer) && symbol.nil?
-      array = to_a
       array.my_each { |num| accumulator = yield(accumulator, num) }
       accumulator
     elsif accumulator.nil? && symbol.nil?
-      array = to_a
       accumulator = array.shift
       array.my_each do |num|
-        accumulator = yield(accumulator, num)
+      accumulator = yield(accumulator, num)
       end
+      accumulator
     end
-    accumulator
   end
 end
 
 def multiply_els(array)
   array.my_inject { |product, value| p product * value }
 end
+
+#TEST CASES INJECT - MY_INJECT
+
+p "-----------------------INJECT-------------------------"
+# Sum some numbers
+p (5..10).inject(:+)                             #=> 45
+# Same using a block and inject
+p (5..10).inject { |sum, n| sum + n }            #=> 45
+# Multiply some numbers
+p (5..10).inject(1, :*)                          #=> 151200
+# Same using a block
+p (5..10).inject(1) { |product, n| product * n } #=> 151200
+# find the longest word
+longest = %w{ cat sheep bear }.inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+p longest                                        #=> "sheep"
+
+p "-----------------------MY_INJECT-------------------------"
+# Sum some numbers
+p (5..10).my_inject(:+)                             #=> 45
+# Same using a block and inject
+p (5..10).my_inject { |sum, n| sum + n }            #=> 45
+# Multiply some numbers
+p (5..10).my_inject(1, :*)                          #=> 151200
+# Same using a block
+p (5..10).my_inject(1) { |product, n| product * n } #=> 151200
+# find the longest word
+longest = %w{ cat sheep bear }.my_inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+p longest                                        #=> "sheep"
